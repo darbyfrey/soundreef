7ce24cf6af95bae86a4dcf29eb6097db5e27c040
Darby Frey <darby@electricshoes.com>
2014-01-26 08:55:51 -0600

    Added support for new Earnings.list endpoint

--- lib/soundreef/earnings.rb
+++ lib/soundreef/earnings.rb
@@ -1,5 +1,9 @@
 class Soundreef
   class Earnings
+    def self.list(params = {})
+      Soundreef::Request.new('/earnings/list', params).response
+    end
+
     def self.get(params = {})
       Soundreef::Request.new('/earnings/get', params).response
     end
